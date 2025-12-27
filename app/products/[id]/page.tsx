fbca469bfc6f65e0e7240f8f2a5de367b29973d8
Satnam Singh <122001110+Satnam-Gill@users.noreply.github.com>
2025-12-28 04:36:00 +0530

    Add: SSG

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -4,33 +4,6 @@ import ProductDetails from '@/components/products/ProductDetails';
 
 export const revalidate = 3600;
 
-export const dynamicParams = true;
-
-
-export async function generateStaticParams() {
-  try {
-    const res = await fetch('https://fakestoreapi.com/products', {
-      next: { revalidate: 3600 }, // IMPORTANT for Vercel
-    });
-
-    if (!res.ok) {
-      console.error(
-        `Failed to fetch products: ${res.status} ${res.statusText}`
-      );
-      return [];
-    }
-
-    const products = await res.json();
-
-    return products.map((product: { id: number }) => ({
-      id: product.id.toString(),
-    }));
-  } catch (error) {
-    console.error('Failed to generate static params:', error);
-    return [];
-  }
-}
-
 export default async function ProductDetailPage({
   params,
 }: {
